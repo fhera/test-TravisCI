4a39021b0d4b7a12285a55df42e6bcb2ef149d7e
fernando <fheraa@gmail.com>
2019-03-06 12:06:44 +0100

    Add coveraged

--- src/main/java/com/barlingo/models/entities/User.java
+++ src/main/java/com/barlingo/models/entities/User.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Version;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
